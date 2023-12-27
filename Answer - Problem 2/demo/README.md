4951a24e6613a9ef528a16e46ae0bf8ddf4209dc
Julian <julian.camargo@sitrack.com>
2023-12-27 09:28:17 -0300

    chore: resolved problem 2

---
+++ Answer - Problem 2/demo/README.md
@@ -0,0 +1,24 @@
+# 2. Question 2
+
+Given an integer arraym numbers, count the number of elements that occur more than once.
+
+**Example**
+
+numbers = [1, 3, 3, 4, 4, 4]
+
+**Function description**
+
+Complete the function `countDuplicate` in the editor.
+
+`countDuplicate` hast the following parameter(s):
+* `int numbers[n]`: an array of integers
+
+*Returns*:
+* `int`: an integer that denotes the number of non-unique values in the numbers array
+
+**Constraints**
+
+* 3 $\leq$ n $\leq$ 1000
+* 1 $\leq$ numbers[i] $\leq$ 1000
+* 0 $\leq$ i $\leq$ n
+
---
+++ Answer - Problem 2/demo/src/main/java/com/hackerrank/demo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.hackerrank.demo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		System.out.println("Duplicated Numbers: " + countDuplicate(numbers));
+
+		numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 1,9);
+		System.out.println("Duplicated Numbers: " + countDuplicate(numbers));
+
+	}
+
+	public static int countDuplicate(List<Integer> numbers) {
+		List<Integer> sortedNumbers = numbers.stream().sorted().collect(Collectors.toList());
+		Integer repeatedValues = 0;
+		Integer lastValue = null; //= numbers.get(0);
+		for (int i = 0; i < sortedNumbers.size() - 1; i++) {
+			if(sortedNumbers.get(i) == lastValue) {
+				continue;
+			}
+			if(sortedNumbers.get(i) == sortedNumbers.get(i+1)) {
+				repeatedValues++;
+				lastValue = sortedNumbers.get(i);
+			}
+		}
+		return repeatedValues;
+	}
+
+	// https://www.youtube.com/watch?v=9_R6xZpLqGc
+
+}
